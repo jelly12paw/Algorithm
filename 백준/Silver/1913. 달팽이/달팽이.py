cf485915feb3768587543983b157ba3b3c01ac12
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2023-01-30 16:25:30 +0900

    [Silver III] Title: 달팽이, Time: 796 ms, Memory: 70000 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,59 +1,36 @@
-import sys
+n = int(input())
+find_ = int(input())
+lists_ = [[0] * n for i in range(n)]
 
-input = sys.stdin.readline
+k = n ** 2
 
-# 아래, 오른쪽, 위로, 왼쪽 순서
+d = -1
+x, y = -1, 0
+# 입력받은 숫자의 위치값
+fx, fy = 0, 0
+
+# 방향 설정 아래 오른쪽 위 왼쪽
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-# 입력
-n = int(input())
-
-# 찾는 수 입력
-f_n = int(input())
-
-# 달팽이 를 구현 하기 위한 이중배열
-arr = [[0] * n for _ in range(n)]
-
-
-# 방향성의 규칙에 의한 -1 선언
-d = -1
-
-# n * n 의 값부터 시작!
-k = n * n
-
-# 0,0 이 아닌 -1, 0 으로 배정이후 규칙성에 넣기
-x, y = -1, 0
-
-# 찾는 숫자 값 넣기 위한 배열
-ans = []
-
-# 모든 달팽이는 n * 2 - 1 횟수 안에 다 끝난다.
-for _ in range(n * 2 - 1):
-    # 방향
+for _ in range(2 * n - 1):
     d = (d + 1) % 4
 
-    # n번 만큼 움직인다.
-    for _ in range(n):
-
-        # 규칙성에 의해 값 플러스 하기
+    for i in range(n):
         x += dx[d]
         y += dy[d]
+        lists_[x][y] = k
 
-        # 해당 위치 k값으로 정의
-        arr[x][y] = k
-
-        # 만약 k가 찾는 수라면
-        if k == f_n:
-            # 배열에 넣기
-            ans.append((x + 1, y + 1))
-        # k -= 1 진행하므로 달팽이 움직임을 보여주기
+        if k == find_:
+            fx, fy = x + 1, y + 1
+        
         k -= 1
-    # 만약 아래로 내려가는 것 또는 위로 올라 가는 것이 끝났다면 n을 -1 더해서 n번 을 줄이기
+
+    # d의 값이 0 혹은 2라면 (아래와 위 방향이라면)
     if not d or not d % 2:
         n -= 1
-# 반복문을 이용하여 해당 인덱스 값을 Unpacking 통해 출력
-for i in range(len(arr)): print(*arr[i])
 
-# 위치값 Unpacking으로 출력하기
-print(*ans[0])
+for j in lists_:
+    print(*j)
+
+print(fx, fy)
