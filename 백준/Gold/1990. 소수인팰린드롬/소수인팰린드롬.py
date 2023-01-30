f95f1ce7280335b437e1f5fe14dfc85cff0665b5
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2023-01-30 16:46:00 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 2072 ms, Memory: 358988 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.py
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.py
@@ -1,26 +1,21 @@
-p = 10 ** 7
-
-prime = [False, False] + [True] * (p - 1)
-
-for i in range(2, p + 1):
-    if prime[i]:
-        for j in range(i * 2, p + 1, i):
-            prime[j] = False
-
 a, b = map(int, input().split())
 
-if b > 10000000:
-    b = 10000000
+# 천만 이상이 되면 소수이면서 팰린드롬 수는 존재하지 않는다.
+if b > 10 ** 7:
+    b = 10 ** 7
 
-nums = [ i for i in range(a, b + 1)]
+# 에라토스테네스의 체
 
-numbers = []
-for i in nums:
+prime = [False, False] + [True] * (b - 1)
+
+for i in range(2, b + 1):
     if prime[i]:
-        numbers.append(i)
+        for j in range(i * 2, b + 1, i):
+            prime[j] = False
 
-for i in numbers:
-    if str(i) == str(i)[::-1]:
-        print(i)
+for k in range(a, b + 1):
+    if prime[k]:
+        if str(k) == str(k)[::-1]:
+            print(k)
 
 print(-1)
