357ecc416b651ec36eabaad0bdd5fd28a4d1f71a
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2023-02-12 20:18:58 +0900

    [Gold V] Title: 탑, Time: 640 ms, Memory: 130984 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,14 @@
+n = int(input())
+top = list(map(int, input().split()))
+stack = []
+answer = [0] * n
+
+for k, v in enumerate(top):
+    while stack:
+        if stack[-1][1] > v:
+            answer[k] = stack[-1][0] + 1
+            break
+        else:
+            stack.pop()
+    stack.append((k, v))
+print(*answer)
