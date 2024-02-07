a9742f9145eff0e7bde514595a4eae41530f4b5d
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-02-07 21:48:33 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,9 @@
+function solution(x, y) {
+  const A = -2;
+  const B = x + 4;
+  const C = -2 * x - 2 * y;
+  const discriminant = B * B - 4 * A * C;
+  const root1 = (-B + Math.sqrt(discriminant)) / (2 * A);
+  const root2 = (-B - Math.sqrt(discriminant)) / (2 * A);
+  return [root2,root1];
+}
