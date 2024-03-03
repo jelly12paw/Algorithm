af418a3f3944ab38dbd70ad97b74b8729fc55cf0
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-03-03 22:07:54 +0900

    [level 2] Title: 조이스틱, Time: 1.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,20 @@
+from collections import deque
+from itertools import product
+
+def solution(name):    
+    results = []
+
+    for rs in product((-1,1), repeat=len(name)-1):
+        name_deque = deque(name)
+        default = deque('A'*len(name))
+
+        for c, r in enumerate([0]+list(rs)):
+            default.rotate(r)
+            name_deque.rotate(r)
+            default[0] = name_deque[0]
+
+            if name_deque == default:
+                results.append(c)
+                break
+
+    return min(set(results))+sum([min(ord(l)-ord('A'), ord('Z')-ord(l)+1) for l in name])
