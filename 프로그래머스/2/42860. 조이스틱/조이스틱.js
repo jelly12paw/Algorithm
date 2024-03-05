9ee8a726d0977ba88bb5af7e47d0cd30aed4d50a
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-03-05 10:59:37 +0900

    [level 2] Title: 조이스틱, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -0,0 +1,28 @@
+function solution(name) {
+  var answer = 0;
+  let min = name.length - 1;
+
+  for (let i = 0; i < name.length; i++) {
+    let currentAlPhabet = name.charCodeAt(i);
+
+    if (currentAlPhabet < 78) {
+      answer += currentAlPhabet % 65;
+    } else {
+      answer += 91 - currentAlPhabet;
+    }
+
+    let nextIndex = i + 1;
+
+    while (nextIndex < name.length && name.charCodeAt(nextIndex) === 65) {
+      nextIndex += 1;
+    }
+
+    min = Math.min(
+      min,
+      i * 2 + name.length - nextIndex, 
+      i + (name.length - nextIndex) * 2
+    );
+  }
+  answer += min;
+  return answer;
+}
