7684f57c71145a4f84906fd7634e701dd3ed9d42
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-03-26 00:32:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> [Int] {
+    var odd_num = 0;
+    var even_num = 0;
+    for i in num_list {
+        if i % 2 == 0 {
+            even_num += 1
+        } else {
+            odd_num += 1
+        }
+    }
+    var answer = [even_num, odd_num];
+    return answer
+}
