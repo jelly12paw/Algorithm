b4323d492e1a0e28dea03ccc6f918f9b6edf4499
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-03-28 17:02:27 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    var answer = 0;
+    if dot[0] > 0 {
+        if dot[1] > 0 {
+            answer = 1;
+        } else {
+            answer = 4;
+        }
+    } else {
+        if dot[1] < 0 {
+            answer = 3;
+        } else {
+            answer = 2;
+        }
+    }
+    return answer
+}
