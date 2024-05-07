9c035582a0dda9c1adf8dd90a89f68ec02f196da
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-05-07 21:23:38 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ hp:Int) -> Int {
+    var hp = hp
+    var result = 0
+    for i in [5, 3, 1] {
+        result += hp / i
+        hp %= i
+    }
+    return result
+}
