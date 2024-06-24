458ff972d6c62c78707f580ab8b6d9db06006876
HJ Park <107910587+jelly12paw@users.noreply.github.com>
2024-06-25 01:14:21 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    var result = ""
+    for c in my_string {
+        if !result.contains(c) {
+            result += String(c)
+        }
+    }
+    return result
+}
